0f721b46578b2c2f722968931b6b9c41856b1a44
Manoel Tindão <timdao02@hotmail.com>
2020-06-10 14:20:25 -0300

    Comentários e variáveis e constantes

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,35 @@
 package fundamentos;
 
+/**
+ * Essa classe representa o java doc
+ * 
+ * @author Manoel Ozeias Morais da Silva <manoeloms@gmail.com>
+ * @since JDK1.0
+ * @see
+ */
+
 public class PrimeiroPrograma 
 {
 	public static void main(String[] args)
 	{
+		// Uma sentença de dócido termina com ;
 		System.out.println("Primeiro Programa!");
+		
+		/*
+		 * Linha1
+		 * Linha2
+		 */
+		
+		
+		/*
+		 
+		Linha 1 
+		Linha 2		 
+		 
+		 */
+		
+		System.out.println("Fim"); // Aqui também funciona
+		
+		
 	}
 }
